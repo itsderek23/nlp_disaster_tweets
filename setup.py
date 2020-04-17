b76c316de761b39808be2815d884e8b0f8eddaa9
Derek Haynes <derek.haynes@gmail.com>
2020-04-17 11:56:28 -0600

    Cookiecutter strucutre and notebook file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementing a simplified version of CD4ML based on a Kaggle notebook from the "Real or Not? NLP with Disaster Tweets" competition.',
+    author='Derek Haynes',
+    license='MIT',
+)
