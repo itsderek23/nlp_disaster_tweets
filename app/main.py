73121e18b900c1e82828413aa8aa10f445bb1130
Derek Haynes <derek.haynes@gmail.com>
2020-04-22 07:20:05 -0600

    Moving init outside of main

--- app/main.py
+++ app/main.py
@@ -3,9 +3,14 @@ from src.models.disaster_tweets_model import DisasterTweetsModel
 import sys
 import os
 
+def dvc_pull():
+    os.system("git init") # The deployed app isn't a git repo but needs to be for dvc
+    os.system("dvc pull prepare_train_submit.dvc")
+
 # initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
-model = None
+dvc_pull()
+model = DisasterTweetsModel()
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -14,11 +19,5 @@ def predict():
     sys.stdout.flush()
     return flask.jsonify(result.tolist())
 
-def dvc_pull():
-    os.system("git init") # The deployed app isn't a git repo but needs to be for dvc
-    os.system("dvc pull prepare_train_submit.dvc")
-
 if __name__ == "__main__":
-    dvc_pull()
-    model = DisasterTweetsModel()
     app.run()
